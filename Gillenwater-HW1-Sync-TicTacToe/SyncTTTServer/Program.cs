9f9f280724acf22ff67ea7a4b174028eb255e9c1
Jake Gillenwater <JakeGill70@gmail.com>
2020-09-11 17:01:32 -0400

    Rewrote server main function to use the SocketFacade class

--- Gillenwater-HW1-Sync-TicTacToe/SyncTTTServer/Program.cs
+++ Gillenwater-HW1-Sync-TicTacToe/SyncTTTServer/Program.cs
@@ -8,6 +8,45 @@ namespace SyncTTTServer
 {
     class Program
     {
+
+        public static void BetterMain(string[] args) {
+
+            Console.WriteLine(ProgramMeta.getProgramHeaderInfo());
+
+
+            // Establish a server connection point
+            try
+            {
+                SocketFacade socket = new SocketFacade(SharedResources.ProgramMeta.PORT_NUMBER);
+                socket.EstablishLocalEndpoint();
+                socket.BindSocket();
+                socket.ListenForIncomingConnections();
+
+                while (true) {
+                    // Listen for incoming connections
+                    socket.AcceptIncomingConnection();
+                    string requestData = socket.ReadData();
+
+                    // Process Data
+                    // Display the data
+                    Console.WriteLine("Text received : {0}", requestData);
+
+                    // Echo data back to the client
+                    socket.SendData("ReqRec: " + requestData);
+
+                    // Close the connection
+                    socket.CloseConnection();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("An error occured while trying to establish the server connection point.");
+            }
+
+            Console.WriteLine("\nPress ENTER to exit...");
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine(ProgramMeta.getProgramHeaderInfo());
