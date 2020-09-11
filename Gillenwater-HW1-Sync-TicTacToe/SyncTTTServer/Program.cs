b85340223768474dacccb4437ddb11bf9516c1b1
Jake Gillenwater <JakeGill70@gmail.com>
2020-09-11 19:56:25 -0400

    Replaced the main() function of the server with the contents of BetterMain()
    The server has fully embraced the socket fascade

--- Gillenwater-HW1-Sync-TicTacToe/SyncTTTServer/Program.cs
+++ Gillenwater-HW1-Sync-TicTacToe/SyncTTTServer/Program.cs
@@ -9,21 +9,28 @@ namespace SyncTTTServer
     class Program
     {
 
-        public static void BetterMain(string[] args) {
+        static void Main(string[] args)
+        {
+            // Set console window name
+            Console.Title = "Server";
 
+            // Display some house keeping information
             Console.WriteLine(ProgramMeta.GetProgramHeaderInfo());
 
+            // Declare outside of the try-catch a new socket that will
+            // be used to represent the connection point to the server.
             SocketFacade listener = null;
 
             // Establish a server connection point
             try
             {
+                // Create a socket representing the connection point to the server.
                 listener = new SocketFacade(SharedResources.ProgramMeta.PORT_NUMBER);
-                
                 listener.BindSocket();
                 listener.ListenForIncomingConnections();
 
-                while (true) {
+                while (true)
+                {
                     // Listen for incoming connections
                     SocketFacade handler = listener.AcceptIncomingConnection();
                     string requestData = handler.ReadData();
@@ -41,88 +48,19 @@ namespace SyncTTTServer
             }
             catch (Exception e)
             {
+                // Catch exceptions thrown inside the main server loop so that the program
+                // will fail gracefully
                 Console.Error.WriteLine("An error occured while trying to establish the server connection point.");
                 Console.Error.WriteLine(e);
             }
 
+            // Close the connection
             listener?.CloseConnection();
 
+            // Wait for user input before closing the window
             Console.WriteLine("\nPress ENTER to exit...");
             Console.ReadLine();
+
         }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine(ProgramMeta.getProgramHeaderInfo());
-            // Set console window name
-            Console.Title = "Server";
-
-
-            // 1. Allocate a buffer to store incoming data
-            byte[] bytesBuffer = new byte[1024];
-            string data;
-
-            // 2. Establish a local endpoint for the socket
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint localEndpoint = new IPEndPoint(ipAddress, ProgramMeta.PORT_NUMBER);
-
-            // 3. Create the socket
-            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            try
-            {
-                // 4. Bind the socket to the local endpoint
-                listener.Bind(localEndpoint);
-
-                // 5. Listen for incoming connections
-                const int MAX_CONNECTION_QUEUE_SIZE = 10;
-                listener.Listen(MAX_CONNECTION_QUEUE_SIZE);
-
-                // 6. Enter a loop
-                while (true)
-                {
-                    // Print some debug information each time someone connects
-                    Console.WriteLine("Waiting for a connection...");
-                    Console.WriteLine("IpAddress: " + ipAddress.ToString());
-                    Console.WriteLine("localEndPoint: " + localEndpoint.ToString());
-
-                    // 6.1 Listen for a connection (blocking call)
-                    Socket handler = listener.Accept();
-                    data = "";
-
-                    // 6.2 Process the connection to the read the incoming data
-                    while (true)
-                    {
-                        int bytesReceived = handler.Receive(bytesBuffer);
-                        data += Encoding.ASCII.GetString(bytesBuffer, 0, bytesReceived);
-                        // Exit loop when the delimer "<EOF>" comes in
-                        if (data.IndexOf("<EOF>") > -1)
-                        {
-                            break;
-                        }
-                    }
-                    // 6.3 process the incoming data
-
-                    // Display the data
-                    Console.WriteLine("Text received : {0}", data);
-
-                    // Echo the data back to the client
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
-                    handler.Send(msg);
-
-                    // 6.4 Close the connection
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            Console.WriteLine("\nPress ENTER to exit...");
-            Console.ReadLine();
-        }
-    }
 }
 
