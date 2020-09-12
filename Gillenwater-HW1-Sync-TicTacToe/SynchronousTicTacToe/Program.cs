37a0c9fd195ba04e0b95b9550f1aea720de0c2e5
Jake Gillenwater <JakeGill70@gmail.com>
2020-09-11 20:28:35 -0400

    Replaced the contents of the Main() funtion with BetterMain() so that the client now fully embraces the SocketFacade

--- Gillenwater-HW1-Sync-TicTacToe/SynchronousTicTacToe/Program.cs
+++ Gillenwater-HW1-Sync-TicTacToe/SynchronousTicTacToe/Program.cs
@@ -9,7 +9,12 @@ namespace SyncTTTClient
     class Program
     {
 
-        static void BetterMain() {
+        static void Main(string[] args)
+        {
+
+            // Set console window name
+            Console.Title = "Client";
+
             // Display some house keeping information
             Console.WriteLine(ProgramMeta.GetProgramHeaderInfo());
 
@@ -42,68 +47,13 @@ namespace SyncTTTClient
                 Console.Error.WriteLine("An error occured while trying to establish the server connection point.");
                 Console.Error.WriteLine(e);
             }
-            finally {
+            finally
+            {
                 // Close the connection
                 sender?.CloseConnection();
             }
 
             // Wait for user input before closing the window
-            Console.WriteLine("\nPress ENTER to exit...");
-            Console.ReadLine();
-        }
-
-        static void Main(string[] args)
-        {
-
-            // Set console window name
-            Console.Title = "Client";
-
-            // 1. Allocate a buffer to store incoming data
-            byte[] bytesBuffer = new byte[1024];
-            string data;
-
-            try
-            {
-                // 2. Establish a remote endpoint for the socket
-                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-                IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEndpoint = new IPEndPoint(ipAddress, ProgramMeta.PORT_NUMBER);
-
-                // 3. Create the socket
-                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-                // 4. Connect the socket to the remote endpoint
-                try
-                {
-                    sender.Connect(remoteEndpoint);
-                    Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
-
-                    // 5. Encode the data to be sent
-                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
-
-                    // 6. Send the data through the socket
-                    int bytesSent = sender.Send(msg);
-
-                    // 7. Listen for  the response (blocking call)
-                    int bytesRec = sender.Receive(bytesBuffer);
-
-                    // 8. Process the response
-                    Console.WriteLine("Echoed Test = {0}", Encoding.ASCII.GetString(bytesBuffer, 0, bytesRec));
-
-                    // 9. Close the socket
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
             Console.WriteLine("\nPress ENTER to exit...");
             Console.ReadLine();
         }
