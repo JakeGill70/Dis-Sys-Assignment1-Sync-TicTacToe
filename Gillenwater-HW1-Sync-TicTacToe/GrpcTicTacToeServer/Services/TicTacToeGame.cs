bde70e403489e35a4b07c5a968ca92acf7425e23
Jake Gillenwater <JakeGill70@gmail.com>
2020-10-16 00:04:52 -0400

    Pulled TicTacToeGame into its own file

---
+++ Gillenwater-HW1-Sync-TicTacToe/GrpcTicTacToeServer/Services/TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using SyncTTTServer;
+
+namespace GrpcTicTacToeServer
+{
+    public struct TicTacToeGame {
+        public string playerID;
+        public char currentTurn;
+        public char currentState;
+        public char playerCharacter;
+        public TicTacToeBoard board;
+
+        public TicTacToeGame(string id, char pc) {
+            playerID = id;
+            currentTurn = 'X';
+            currentState = 'N';
+            playerCharacter = pc;
+            board = new TicTacToeBoard();
+        }
+    }
+}
